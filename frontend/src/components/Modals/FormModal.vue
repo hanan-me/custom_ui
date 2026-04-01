e79a1069802af46b745cfd98500ad20643e8aa92
hanan-me <142660158+hanan-me@users.noreply.github.com>
2026-04-01 16:03:30 +0500

    Update and rename FormModel.vue to FormModal.vue

--- frontend/src/components/Modals/FormModal.vue
+++ frontend/src/components/Modals/FormModal.vue
@@ -6,7 +6,7 @@
       <div class="mb-5 flex items-center justify-between">
         <div>
           <h3 class="text-2xl font-semibold leading-6 text-ink-gray-9">
-            {{ __('Create CRM Doc') }}
+            {{ docType }}
           </h3>
         </div>
       </div>
